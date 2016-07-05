474d6e5a943cce41ee94aa44909939f9d93b91b9
cha <user@125.137.107.134>
2016-07-05 20:19:14 +0900

    no message

---
+++ Practice3_jump/Assets/Script/CharacterControler.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControler : MonoBehaviour {
+
+    public float speed = 3f;
+    public float jumpPower = 5.0f;
+
+    Rigidbody rigdbody;
+    Animator animator;
+    Vector3 movement;
+
+    float horizontalMove;
+    float verticalMove;
+    bool isJumping;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Awake()
+    {
+        rigdbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+    //키 입력
+	void Update () {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        AnimationUpdate();
+	}
+
+    //물리적 처리
+    void FixedUpdate ()
+    {
+        //Run();
+        Jump();
+    }
+
+    void AnimationUpdate()
+    {
+        if (isJumping == true) { 
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
+    }
+
+
+
+
+
+
+
+
+    void Run()
+    {
+        movement.Set(horizontalMove, 0, verticalMove);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rigdbody.MovePosition(transform.position + movement);
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            return;
+        }
+
+        //rigdbody.MovePosition(transform.position + Vector3.up);
+        rigdbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        isJumping = false;
+
+        
+    }
+}
